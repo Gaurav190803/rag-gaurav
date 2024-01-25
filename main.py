001fd667c05a4f8c69286ab045950a5ac021b5cf
GauravML123 <gaurav.a@w3dev.email>
2024-01-25 17:02:52 +0530

    updated main,py

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import openai
 from dotenv import load_dotenv
 import os
 import streamlit as st
+from utils import *
 
-load_dotenv()
-openai.api_key = os.getenv("api_key")
 output = {
         "choices": [
             {
@@ -16,27 +15,14 @@ output = {
     }
 with st.sidebar:
     with st.form(key = 'querySubmission'):
-        context = st.text_area(
-            "CONTEXT",
-            placeholder="Enter the context paragraph",
+        prompt = st.text_area(
+            "QUESTION",
+            placeholder="Enter question",
             key="1",
         )
-        query = st.text_area(
-            "QUERY",
-            placeholder="Enter the query",
-            key="2",
-        )
-
         submit = st.form_submit_button()
         
-        if context and query:
-            output = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo-16k",
-                temperature=0.6,
-                messages=[
-                    {"role": "system", "content": context},
-                    {"role": "user", "content": query},
-                ],
-            )
+        if prompt:
+            output = QNA(prompt)
     
-st.markdown(f'{output["choices"][0]["message"]["content"]}</div>', unsafe_allow_html=True)
+st.markdown(f'{output}</div>', unsafe_allow_html=True)
--- utils.py
+++ utils.py
@@ -1,11 +1,5 @@
-from langchain import hub
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import WebBaseLoader
-from langchain_community.vectorstores import Chroma
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 import PyPDF2
 from sentence_transformers import SentenceTransformer
 from dotenv import main
